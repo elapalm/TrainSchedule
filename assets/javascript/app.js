61490e0dd3cb081af340cb8464eefa7f50ca1db6
emma lapalm <lillypadcrafts@gmail.com>
2019-03-14 22:36:58 -0700

    start js

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBGm-3rFsQ16L3VjOqwR3mKtVrkd_QopnQ",
+    authDomain: "trainschedule-a32ce.firebaseapp.com",
+    databaseURL: "https://trainschedule-a32ce.firebaseio.com",
+    projectId: "trainschedule-a32ce",
+    storageBucket: "trainschedule-a32ce.appspot.com",
+    messagingSenderId: "904088002713"
+  };
+  firebase.initializeApp(config);
+
+  
